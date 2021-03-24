a0f84614e28312ac5ff1d2b168167501d5be2ef3
lovesaikrishna <44071983+lovesaikrishna@users.noreply.github.com>
2021-03-24 18:01:10 -0400

    Create CronScript.sh
    Initial commit CronScript.sh

---
+++ CronScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+/usr/bin/echo "   " > /var/tmp/CronScript.txt
+/usr/bin/echo "Bingo!! CronJob Deployed Successfully" >> /var/tmp/CronScript.txt
+/usr/bin/echo "   " >> /var/tmp/CronScript.txt
+/usr/bin/hostname -f  >> /var/tmp/CronScript.txt
+/usr/bin/echo "   " >> /var/tmp/CronScript.txt
+/usr/bin/uname -a >> /var/tmp/CronScript.txt
+/usr/bin/echo "   " >> /var/tmp/CronScript.txt
